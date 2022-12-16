b40b77044c9d086efb6388cef2a313bcb45ada42
Ezequiel Cabello <cabello.marcos.ezequiel@gmail.com>
2022-12-15 22:10:00 -0300

    Problemas en el MainSecurity

---
+++ TFIMEC/BackEnd/src/main/java/com/portfolio/mec/Security/MainSecurity.java
@@ -0,0 +1,35 @@
+package com.portfolio.mec.Security;
+
+import com.portfolio.mec.Security.Service.UserDetailsImpl;
+import com.portfolio.mec.Security.jwt.JwtEntryPoint;
+import com.portfolio.mec.Security.jwt.JwtTokenFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class MainSecurity extends WebSecurityConfigurerAdapter {
+    @Autowired
+    UserDetailsImpl userDetailsServicesImpl;
+    @Autowired
+    JwtEntryPoint jwtEntryPoint;
+    
+    @Bean
+    public JwtTokenFilter jwtTokenFilter(){
+        return new JwtTokenFilter();
+    }
+    
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+    
+    
+    
+}
